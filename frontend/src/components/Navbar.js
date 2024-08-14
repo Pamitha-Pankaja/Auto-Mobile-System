c8ba722f11b836085ffb4278e5c29e6a6fef47fe
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2024-08-14 21:55:51 +0530

    updated mobile nav

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,6 +8,7 @@ import { AuthContext } from './AuthForms/AuthContext';
 const Navbar = () => {
     const { state, dispatch } = useContext(AuthContext);
     const [activeLink, setActiveLink] = useState('home');
+    const [isCollapsed, setIsCollapsed] = useState(true);
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -37,9 +38,12 @@ const Navbar = () => {
         navigate(`/#${section}`);
     };
 
+    const handleToggle = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
     const handleLogout = () => {
         dispatch({ type: 'LOGOUT' });
-        // Additional actions on logout if needed
     };
 
     return (
@@ -51,20 +55,21 @@ const Navbar = () => {
                             <Link className="navbar-brand pt-0" to="/">
                                 <img src={logo} alt="Logo" className="img-fluid" />
                             </Link>
-                            <button
-                                className="navbar-toggler collapsed"
+                            <div
+                                className={`navbar-toggler ${isCollapsed ? 'collapsed' : ''}`}
                                 type="button"
-                                data-toggle="collapse"
+                                onClick={handleToggle}
+                                // data-toggle="collapse"
                                 data-target="#navbarSupportedContent"
                                 aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
+                                aria-expanded={!isCollapsed}
                                 aria-label="Toggle navigation"
                             >
                                 <span className="navbar-toggler-icon"></span>
                                 <span className="navbar-toggler-icon"></span>
                                 <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            </div>
+                            <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarSupportedContent">
                                 <ul className="navbar-nav mr-auto">
                                     <li className={`nav-item ${activeLink === 'home' ? 'active' : ''}`}>
                                         <ScrollLink
@@ -106,18 +111,18 @@ const Navbar = () => {
                                             Contact
                                         </ScrollLink>
                                     </li>
+                                    <div className="btn-talk ml-auto">
+                                        <ul className="m-0 p-0">
+                                            <li className="list-unstyled d-lg-inline-block">
+                                                <Link className="nav-link" to="/appointment">
+                                                    Book Now<i className="fa-solid fa-arrow-right"></i>
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </ul>
-                                <div className="btn-talk ml-auto">
-                                    <ul className="m-0 p-0">
-                                        <li className="list-unstyled d-lg-inline-block">
-                                            <Link className="nav-link" to="/appointment">
-                                                Book Now<i className="fa-solid fa-arrow-right"></i>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="btn-login ml-auto d-flex align-items-center justify-content-end navbar-end">
 
+                                <div className="btn-login ml-auto d-flex align-items-center">
                                     {state.user ? (
                                         <>
                                             <span className="navbar-text mr-3">Hello, {state.user.username}</span>
@@ -125,14 +130,14 @@ const Navbar = () => {
                                                 Logout
                                             </button> */}
                                             <div className="ml-auto">
-                                            <ul className="m-0 p-0">
-                                                <li className="list-unstyled d-lg-inline-block">
-                                                    <Link className="btn btn-outline-primary" onClick={handleLogout}>
-                                                      Logout
-                                                    </Link>
-                                                </li>
-                                            </ul>
-                                        </div>
+                                                <ul className="m-0 p-0">
+                                                    <li className="list-unstyled d-lg-inline-block">
+                                                        <Link className="btn btn-outline-primary" onClick={handleLogout}>
+                                                            Logout
+                                                        </Link>
+                                                    </li>
+                                                </ul>
+                                            </div>
                                         </>
                                     ) : (
                                         <div className="ml-auto">
