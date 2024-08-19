68897a3dd6f8acafd5c331ee6617f82c157ad833
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2024-08-19 11:52:01 +0530

    updated the auth

--- frontend/src/components/AuthForms/AuthContext.js
+++ frontend/src/components/AuthForms/AuthContext.js
@@ -2,20 +2,26 @@ import React, { createContext, useReducer } from 'react';
 
 // Initial state
 const initialState = {
-  user: null,
-  token: null,
+  user: JSON.parse(localStorage.getItem('user')) || null,
+  token: localStorage.getItem('token') || null,
 };
+
 
 // Reducer function
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'LOGIN_SUCCESS':
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('token', action.payload.token);
+      console.log(localStorage.getItem('user'));
       return {
         ...state,
         user: action.payload.user,
         token: action.payload.token,
       };
     case 'LOGOUT':
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
       return initialState;
     default:
       return state;
