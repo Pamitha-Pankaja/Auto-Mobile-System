73f88e613b11333bb1db847f995f48d959823ac5
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2024-08-20 11:57:11 +0530

    updated

--- frontend/src/components/HomeSections/AboutUs.js
+++ frontend/src/components/HomeSections/AboutUs.js
@@ -24,7 +24,7 @@ const AboutUs = () => {
             <div className="about-content">
               <h6 className="about-text" data-aos="fade-up" data-aos-anchor-placement="top-bottom">About Us</h6>
               <h2>Who We Are</h2>
-              <p className="mb-2 about-padding">At AUTORIX, we’ve been dedicated to providing exceptional vehicle care for over 50 years. Our team of skilled technicians combines expertise with a passion for automotive excellence, ensuring your vehicle runs smoothly and safely. From routine maintenance to complex repairs, we pride ourselves on delivering top-notch service with integrity and precision. Trust us to keep your vehicle in peak condition—because we believe every journey deserves the best care.</p>
+              <p className="mb-2 about-padding">At AUTODOCK, we’ve been dedicated to providing exceptional vehicle care for over 50 years. Our team of skilled technicians combines expertise with a passion for automotive excellence, ensuring your vehicle runs smoothly and safely. From routine maintenance to complex repairs, we pride ourselves on delivering top-notch service with integrity and precision. Trust us to keep your vehicle in peak condition—because we believe every journey deserves the best care.</p>
               {/* <p className="mb-4 about-padding">Reprehenderit qui in ea volu velit esse quam nihil moe stiae consequatur, vel illum eui.</p> */}
               <div className="row">
                 <div className="col-lg-6 col-md-6 col-sm-6 mb-4">
