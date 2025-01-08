ee2e4a9427fb5c2381dcbc5734e399df150091ca
Pamitha <89682916+Pamitha-Pankaja@users.noreply.github.com>
2025-01-08 10:40:02 +0530

    appointment.js updated

--- frontend/src/pages/Appointment.js
+++ frontend/src/pages/Appointment.js
@@ -293,6 +293,282 @@
 // export default Appointment;
 
 
+// import React, { useState, useEffect, useContext } from 'react';
+// import AOS from 'aos';
+// import axios from 'axios';
+// import { AuthContext } from '../components/AuthForms/AuthContext';
+// import Navbar from '../components/Navbar';
+// import missionManImg from '../assets/images/our-mission-man-img.png';
+// import { useNavigate } from 'react-router-dom';
+
+
+// const Appointment = () => {
+
+//   let local = "http://localhost:8080";
+//   let deploy = "https://robust-wonder-production.up.railway.app"
+//   let basurl = local;
+//   const navigate = useNavigate(); 
+//   const [selectedCategory, setSelectedCategory] = useState('');
+//   const [selectedCategoryList, setSelectedCategoryList] = useState([]);
+//   const [selectedService, setSelectedService] = useState('');
+//   const [availableServicesList, setAvailableServicesList] = useState([]);
+//   const [selectedavailableTimeSlots, setSelectedavailableTimeSlots] = useState('');
+//   const [availableServices, setAvailableServices] = useState([]);
+//   const [selectedDate, setSelectedDate] = useState('');
+//   const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
+//   const [carType, setCarType] = useState('');
+//   const [carMake, setCarMake] = useState('');
+//   const [carModelType, setCarModelType] = useState('');
+//   const [vehicleNo, setVehicleNo] = useState('');
+
+//   const { state } = useContext(AuthContext);
+//   const { user, token } = state;
+
+//   useEffect(() => {
+//     console.log("user",user);
+//     if(token === null){
+//       navigate('/signin');
+//     }   
+//     fetchCategories();
+//   }, []);
+
+//   useEffect(() => {  
+//     if (selectedCategory) {
+//       fetchServices(selectedCategory);
+//     }
+//   }, [selectedCategory]);
+
+//   useEffect(() => {
+//     if (selectedDate) {
+//       fetchAvailableTimeSlots(selectedDate);
+//     }
+//   }, [selectedDate]);
+
+//   const fetchCategories = async () => {
+//     try {
+//       const response = await axios.get(basurl+'/api/service-categories');
+//       setSelectedCategoryList(response.data);
+//     } catch (error) {
+//       console.error('Error fetching categories:', error);
+//     }
+//   };
+
+//   const fetchServices = async (selectedCategory) => {
+//     try {
+//       const response = await axios.get(basurl+'/api/services/category/'+selectedCategory.id);
+//       setAvailableServicesList(response.data);
+//     } catch (error) {
+//       console.error('Error fetching categories:', error);
+//     }
+//   };
+
+//   const fetchAvailableTimeSlots = async (selectedDate) => {
+//     try {
+//       let url = `${basurl}/api/appointments/available-slots?date=${selectedDate}&serviceId=${selectedService.id}`;
+//       const response = await axios.get(url);
+//       console.log(response);
+//       setAvailableTimeSlots(response.data);
+//     } catch (error) {
+//       console.error('Error fetching time slots:', error);
+//     }
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const appointmentData = {
+//       vehicle: { 
+//         make: carMake,
+//         model: carModelType,
+//         vehicleNo: vehicleNo,
+//         type: carType,
+//         owner: { id: user.id }
+//       },
+//       technician: { id: 3 },  // Replace with the actual technician ID
+//       service: { id: selectedService.id },
+//       user: { id: user.id },
+//       date: selectedDate,
+//       startTime: selectedavailableTimeSlots  // Assuming this holds start time 
+//     };
+
+//     try {
+//       const response = await axios.post(`${basurl}/api/appointments/book`, appointmentData);
+//       console.log('Appointment booked successfully:', response.data);
+//       // Handle successful booking (e.g., show a success message, reset the form, etc.)
+//     } catch (error) {
+//       console.error('Error booking appointment:', error);
+//       // Handle errors (e.g., show an error message)
+//     }
+//   }
+  
+//   return (
+//     <div className="mission-section overflowY-auto">
+//       <div className="container">
+//         <div className="row">
+//           <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 d-xl-block d-none mr-4">
+//             <figure className="our-mission-figure-box" data-aos="fade-left">
+//               <img src={missionManImg} alt="" />
+//             </figure>
+//           </div>
+//           <div className="col-xl-9 col-lg-6 col-md-6 col-sm-12">
+//             <div className="mission-red-box" data-aos="fade-down">
+//               <h5>Make An Appointment</h5>
+//               <form onSubmit={handleSubmit}>
+//                 {/* Service Category and Service Type */}
+//                 <div className="row mb-3">
+//                   <div className="col-md-6">
+//                     <label htmlFor="serviceCategory">Service Category</label>
+//                     <select
+//                       id="serviceCategory"
+//                       className="form-control"
+//                       value={selectedCategory.name}
+//                       onChange={(e) => {
+//                         const selectedOption = e.target.options[e.target.selectedIndex];
+//                         const selectedId = selectedOption.id;
+//                         const selectedValue = selectedOption.value;
+//                         setSelectedCategory({ id: selectedId, name: selectedValue })
+//                       }}
+//                     >
+//                       <option value="">Select Category</option>
+//                       {selectedCategoryList.map((category) => (
+//                         <option id={category.id} key={category.id} value={category.name}>
+//                           {category.name}
+//                         </option>
+//                       ))}
+//                     </select>
+//                   </div>
+//                   <div className="col-md-6">
+//                     <label htmlFor="serviceType">Service Type</label>
+//                     <select
+//                       id="serviceType"
+//                       className="form-control"
+//                       value={selectedService.name}
+//                       onChange={(e) => {
+//                         const selectedOption = e.target.options[e.target.selectedIndex];
+//                         const selectedId = selectedOption.id;
+//                         const selectedValue = selectedOption.value;
+//                         setSelectedService({ id: selectedId, name: selectedValue })
+//                       }}
+//                       disabled={!selectedCategory}
+//                     >
+//                       <option value="">Select Service</option>
+//                       {availableServicesList.map((service) => (
+//                         <option id={service.id} key={service.id} value={service.name}>
+//                           {service.name}
+//                         </option>
+//                       ))}
+//                     </select>
+//                   </div>
+//                 </div>
+
+//                 {/* Date Selection and Available Time Slots */}
+//                 <div className="row mb-3">
+//                   <div className="col-md-6">
+//                     <label htmlFor="appointmentDate">Date</label>
+//                     <input
+//                       type="date"
+//                       id="appointmentDate"
+//                       className="form-control"
+//                       value={selectedDate}
+//                       onChange={(e) => setSelectedDate(e.target.value)}
+//                       disabled={!selectedService}
+//                     />
+//                   </div>
+//                   <div className="col-md-6">
+//                     <label htmlFor="timeSlot">Available Time Slots</label>
+//                     <select
+//                       id="timeSlot"
+//                       className="form-control"
+//                       value={selectedavailableTimeSlots}
+//                       onChange={(e) => setSelectedavailableTimeSlots(e.target.value)}
+//                       disabled={!selectedDate || !selectedService}
+//                     >
+//                       <option value="">Select Time Slot</option>
+//                       {availableTimeSlots.map((slot) => (
+//                         <option key={slot} value={slot}>
+//                           {slot}
+//                         </option>
+//                       ))}
+//                     </select>
+//                   </div>
+//                 </div>
+
+//                 {/* Car Details Section */}
+//                 <h5>Car Details</h5>
+//                 <div className="row mb-3">
+//                   <div className="col-md-6">
+//                     <label htmlFor="carType">Car Type</label>
+//                     <select
+//                       id="carType"
+//                       className="form-control"
+//                       value={carType}
+//                       onChange={(e) => setCarType(e.target.value)}
+//                       disabled={!selectedService}
+//                     >
+//                       <option value="">Select Car Type</option>
+//                       <option value="Sedan">Sedan</option>
+//                       <option value="SUV">SUV</option>
+//                       <option value="Truck">Truck</option>
+//                     </select>
+//                   </div>
+//                   <div className="col-md-6">
+//                     <label htmlFor="carMake">Make</label>
+//                     <input
+//                       type="text"
+//                       id="carMake"
+//                       className="form-control"
+//                       placeholder="e.g., Toyota, Mitsubishi"
+//                       value={carMake}
+//                       onChange={(e) => setCarMake(e.target.value)}
+//                       disabled={!selectedService}
+//                     />
+//                   </div>
+//                 </div>
+
+//                 <div className="row mb-3">
+//                   <div className="col-md-6">
+//                     <label htmlFor="carModelType">Model Type</label>
+//                     <input
+//                       type="text"
+//                       id="carModelType"
+//                       className="form-control"
+//                       placeholder="e.g., Corolla, Pajero"
+//                       value={carModelType}
+//                       onChange={(e) => setCarModelType(e.target.value)}
+//                       disabled={!selectedService}
+//                     />
+//                   </div>
+//                   <div className="col-md-6">
+//                     <label htmlFor="vehicleNo">Vehicle Number</label>
+//                     <input
+//                       type="text"
+//                       id="vehicleNo"
+//                       className="form-control"
+//                       placeholder="e.g., ABC-1234"
+//                       value={vehicleNo}
+//                       onChange={(e) => setVehicleNo(e.target.value)}
+//                       disabled={!selectedService}
+//                     />
+//                   </div>
+//                 </div>
+
+//                 <button type="submit" className="btn btn-primary" disabled={!selectedService || !selectedDate || !selectedavailableTimeSlots}>
+//                   Book Appointment
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Appointment;
+
+
+
+
 import React, { useState, useEffect, useContext } from 'react';
 import AOS from 'aos';
 import axios from 'axios';
@@ -300,11 +576,12 @@ import { AuthContext } from '../components/AuthForms/AuthContext';
 import Navbar from '../components/Navbar';
 import missionManImg from '../assets/images/our-mission-man-img.png';
 import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Appointment = () => {
-
   let local = "http://localhost:8080";
-  let deploy = "https://robust-wonder-production.up.railway.app"
+  let deploy = "https://robust-wonder-production.up.railway.app";
   let basurl = local;
   const navigate = useNavigate(); 
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -312,7 +589,6 @@ const Appointment = () => {
   const [selectedService, setSelectedService] = useState('');
   const [availableServicesList, setAvailableServicesList] = useState([]);
   const [selectedavailableTimeSlots, setSelectedavailableTimeSlots] = useState('');
-  const [availableServices, setAvailableServices] = useState([]);
   const [selectedDate, setSelectedDate] = useState('');
   const [availableTimeSlots, setAvailableTimeSlots] = useState([]);
   const [carType, setCarType] = useState('');
@@ -324,10 +600,9 @@ const Appointment = () => {
   const { user, token } = state;
 
   useEffect(() => {
-    console.log("user",user);
-    if(token === null){
+    if (token === null) {
       navigate('/signin');
-    }   
+    }
     fetchCategories();
   }, []);
 
@@ -345,7 +620,7 @@ const Appointment = () => {
 
   const fetchCategories = async () => {
     try {
-      const response = await axios.get(basurl+'/api/service-categories');
+      const response = await axios.get(basurl + '/api/service-categories');
       setSelectedCategoryList(response.data);
     } catch (error) {
       console.error('Error fetching categories:', error);
@@ -354,7 +629,7 @@ const Appointment = () => {
 
   const fetchServices = async (selectedCategory) => {
     try {
-      const response = await axios.get(basurl+'/api/services/category/'+selectedCategory.id);
+      const response = await axios.get(basurl + '/api/services/category/' + selectedCategory.id);
       setAvailableServicesList(response.data);
     } catch (error) {
       console.error('Error fetching categories:', error);
@@ -363,9 +638,8 @@ const Appointment = () => {
 
   const fetchAvailableTimeSlots = async (selectedDate) => {
     try {
-      let url = `${basurl}/api/appointments/available-slots?date=${selectedDate}&serviceId=${selectedService.id}`;
+      const url = `${basurl}/api/appointments/available-slots?date=${selectedDate}&serviceId=${selectedService.id}`;
       const response = await axios.get(url);
-      console.log(response);
       setAvailableTimeSlots(response.data);
     } catch (error) {
       console.error('Error fetching time slots:', error);
@@ -392,14 +666,33 @@ const Appointment = () => {
 
     try {
       const response = await axios.post(`${basurl}/api/appointments/book`, appointmentData);
-      console.log('Appointment booked successfully:', response.data);
-      // Handle successful booking (e.g., show a success message, reset the form, etc.)
+
+      // Show success toast message
+      toast.success('Your appointment has been successfully booked!', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
+
+      // Reset form
+      setSelectedCategory('');
+      setSelectedService('');
+      setSelectedDate('');
+      setSelectedavailableTimeSlots('');
+      setCarType('');
+      setCarMake('');
+      setCarModelType('');
+      setVehicleNo('');
     } catch (error) {
       console.error('Error booking appointment:', error);
-      // Handle errors (e.g., show an error message)
+
+      // Show error toast message
+      toast.error('Error booking appointment. Please try again.', {
+        position: 'top-right',
+        autoClose: 3000,
+      });
     }
-  }
-  
+  };
+
   return (
     <div className="mission-section overflowY-auto">
       <div className="container">
@@ -412,6 +705,7 @@ const Appointment = () => {
           <div className="col-xl-9 col-lg-6 col-md-6 col-sm-12">
             <div className="mission-red-box" data-aos="fade-down">
               <h5>Make An Appointment</h5>
+              
               <form onSubmit={handleSubmit}>
                 {/* Service Category and Service Type */}
                 <div className="row mb-3">
@@ -425,7 +719,7 @@ const Appointment = () => {
                         const selectedOption = e.target.options[e.target.selectedIndex];
                         const selectedId = selectedOption.id;
                         const selectedValue = selectedOption.value;
-                        setSelectedCategory({ id: selectedId, name: selectedValue })
+                        setSelectedCategory({ id: selectedId, name: selectedValue });
                       }}
                     >
                       <option value="">Select Category</option>
@@ -446,7 +740,7 @@ const Appointment = () => {
                         const selectedOption = e.target.options[e.target.selectedIndex];
                         const selectedId = selectedOption.id;
                         const selectedValue = selectedOption.value;
-                        setSelectedService({ id: selectedId, name: selectedValue })
+                        setSelectedService({ id: selectedId, name: selectedValue });
                       }}
                       disabled={!selectedCategory}
                     >
@@ -559,6 +853,7 @@ const Appointment = () => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
